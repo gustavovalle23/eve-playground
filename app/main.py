8a66cc395edd45930cf1859c2e595ac0ed882a31
Gustavo Valle <gustavo.vallerp@hotmail.com>
2025-03-11 09:09:48 +0100

    :books: docs: add eve swagger

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
-from eve import Eve
 import os
+from eve import Eve
+from eve_swagger import get_swagger_blueprint
+from app.auth.auth import hash_passwords
 
 SETTINGS_PATH = os.path.join(os.path.dirname(__file__), "settings.py")
 app = Eve(settings=SETTINGS_PATH)
 
+app.on_insert_users += hash_passwords
+
+app.register_blueprint(get_swagger_blueprint())
+app.config['SWAGGER_INFO'] = {
+    'title': 'Eve Playground API',
+    'version': '1.0',
+    'description': 'Pydantic + Cerberus Eve API',
+    'contact': {
+        'name': 'Gustavo Valle',
+        'email': 'gustavo@devcrafter.io'
+    }
+}
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv("DEBUG") == "True",)
+    app.run(debug=os.getenv("DEBUG") == "True")
